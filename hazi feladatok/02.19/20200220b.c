193606f30c52d118bc96b3e71148586e5745fb86
Polonkai Pál <polonkaipal5@gmail.com>
2020-02-20 14:50:57 +0100

    20200220b kesz

---
+++ hazi feladatok/02.19/20200220b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("n: ");
+    scanf("%d", &n);
+    if (n % 2 == 0) --n;
+
+    for (int i = n; i > 0; i -= 2) printf("%d\n", i);
+    
+    return 0;
+}
