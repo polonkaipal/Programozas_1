f9e08fc5c75e9a05799c4c897ef017a8c9216915
Polonkai Pál <polonkaipal5@gmail.com>
2020-02-20 14:23:42 +0100

    20200213m kesz

---
+++ hazi feladatok/02.19/20200213m.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int szam, db = 0;
+    do
+    {
+        printf("Egesz szam (vege: 0): ");
+        scanf("%d", &szam);
+
+        if (szam > 0)
+        {
+            ++db;
+        }
+        
+    } while (szam != 0);
+
+    printf("Pozitiv elemek szama: %d\n", db);
+
+    return 0;
+}
