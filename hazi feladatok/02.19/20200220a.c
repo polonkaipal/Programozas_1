0d089ca1c3adbe770b7264da9b777bcdc40bf39b
Polonkai Pál <polonkaipal5@gmail.com>
2020-02-20 14:52:39 +0100

    szepites

--- hazi feladatok/02.19/20200220a.c
+++ hazi feladatok/02.19/20200220a.c
@@ -5,10 +5,7 @@ int main()
     int n;
     printf("n: ");
     scanf("%d", &n);
-    for (int i = 4; i < n + 1; i += 4)
-    {
-        printf("%d\n", i);
-    }
+    for (int i = 4; i < n + 1; i += 4) printf("%d\n", i);
     
     return 0;
 }
