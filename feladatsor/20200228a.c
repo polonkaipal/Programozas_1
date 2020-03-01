c83a716f55a9a542abf3f1fce6a88e339e29848b
Pál Polonkai <polonkaipal5@gmail.com>
2020-03-01 21:32:13 +0100

    20200228a.c kesz

---
+++ feladatsor/20200228a.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define FALSE 0
+#define TRUE 1
+
+double get_positive_number()
+{
+    double n;
+    scanf("%lf", &n);
+    while (n < 0)
+    {
+        printf("Adj meg egy pozitiv valos szamot: ");
+        scanf("%lf", &n);
+    }
+
+    return n;
+}
+
+int valid_triangle(int a, int b, int c)
+{
+    if (a + b > c && a + c > b && b + c > a)
+        return TRUE;
+    return FALSE;
+}
+
+int main()
+{
+    printf("Add meg a haromszog elso oldalat: ");
+    double a = get_positive_number();
+    printf("Add meg a haromszog masodik oldalat: ");
+    double b = get_positive_number();
+    printf("Add meg a haromszog harmadik oldalat: ");
+    double c = get_positive_number();
+
+    printf("A haromszog %sszerkesztheto", valid_triangle(a, b, c) ? "" : "nem ");
+
+    return 0;
+}
