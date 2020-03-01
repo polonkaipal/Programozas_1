fe5794e6941ac9f47d7221187646a07ac97e04a0
Pál Polonkai <polonkaipal5@gmail.com>
2020-03-01 21:19:23 +0100

    20200213r.c bovitve

--- feladatsor/20200213r.c
+++ feladatsor/20200213r.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 
+char * get_value_of_index(double index)
+{
+    if (index <= 20.5)
+    {
+        return "sovany";
+    }
+    
+    else if (index >= 20.5 && index <= 26.5)
+    {
+        return "normal";
+    }
+    else if (index >= 26.6 && index <= 31.9)
+    {
+        return "tulsuly";
+    }
+    else if (index >= 32.0 && index <= 36.9)
+    {
+        return "I. foku elhizas";
+    }
+    else if (index >= 37.0 && index <= 41.9)
+    {
+        return "II. foku elhizas";
+    }
+
+    return "III. foku elhizas";
+}
+
 int main()
 {
     float suly;
-    printf("Testsúly (kg): ");
+    printf("Testsuly (kg): ");
     scanf("%f", &suly);
 
     float magassag;
-    printf("Testmagasság (cm): ");
+    printf("Testmagassag (cm): ");
     scanf("%f", &magassag);
 
-    printf("Az On testtömegindexe: %.2f\n", suly / (magassag * magassag / 10000));
+    double index = suly / (magassag * magassag / 10000);
+    printf("\nAz On testtomegindexe: %.1f\n", index);
+    printf("Ennek jelentese: %s\n", get_value_of_index(index));
 
     return 0;
 }
