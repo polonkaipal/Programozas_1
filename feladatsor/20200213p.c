481cac04f202381b2fabe63eef708a6067a26dc6
Pál Polonkai <polonkaipal5@gmail.com>
2020-03-01 20:27:34 +0100

    20200213p.c kesz

---
+++ feladatsor/20200213p.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum = 0, count = 0, n;
+    do
+    {
+        printf("Egesz szam (vege: 0): ");
+        scanf("%d", &n);
+        if (n > 0)
+        {
+            sum += n;
+            ++count;
+        }
+    }while (n != 0);
+
+    if (count == 0)
+    {
+        printf("Hiba: legalabb egy db szamot meg kell adni!\n");
+        return -1;
+    }
+
+    printf("\nA szamok atlaga: %.2lf", (double)sum / count);
+
+    return 0;
+}
