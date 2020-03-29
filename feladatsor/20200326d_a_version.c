5127043be27459b941a6a6c3a9eb338335480b8f
Polonkai Pál <polonkaipal5@gmail.com>
2020-03-29 16:53:24 +0200

    20200326d_a_version kész lottó

---
+++ feladatsor/20200326d_a_version.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int db;
+    puts("Hány db random számot kérsz?");
+    scanf("%d", &db);
+    int random_szamok[db];
+
+    int also_hatar;
+    printf("Alsó határ: ");
+    scanf("%d", &also_hatar);
+    int felso_hatar;
+    printf("Felső határ: ");
+    scanf("%d", &felso_hatar);
+
+    srand(time(NULL));
+    for (int i = 0; i < db; ++i)
+    {
+        random_szamok[i] = (rand() % (felso_hatar - also_hatar + 1)) + also_hatar;
+    }
+
+    printf("A generált számok: ");
+    for (int i = 0; i < db; ++i)
+    {
+        printf("%d%s", random_szamok[i], i + 1 != db ? ", " : "\n");
+    }
+
+    return 0;
+}
