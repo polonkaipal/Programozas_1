0daca2545b292a252d96301fd0d134d35cfcd018
Polonkai Pál <polonkaipal5@gmail.com>
2020-04-03 15:14:15 +0200

    20200331b kész két pont közti távolság

---
+++ feladatsor/20200331b.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct
+{
+    int x;
+    int y;
+} Pont;
+
+double distance(Pont p1, Pont p2)
+{
+    // TODO
+    /*
+        a négyzet függvény miatt az sqrt() függvény
+        negatív értéket nem fog kapni, ezért nem kell az abs() függvény
+     */
+    return sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
+}
+
+int main()
+{
+    Pont a = {1, 2};
+    Pont b = {6, 5};
+
+    printf("A két pont közti távolság: %lf\n", distance(a, b));
+
+    return 0;
+}
